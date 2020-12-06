f33bf738dee27938c712cff6750fa7db081de3d5
jpodonnell <jpodonnell80@gmail.com>
2020-12-06 10:50:03 -0500

    Create day6.sql

---
+++ day6.sql
@@ -0,0 +1,4 @@
+load the file
+sed 's/^$/123thisisanewline123/g' question_answers.input > questions_thisisanewline.input
+cat questions_thisisanewline.input | tr -d '\n' > questions_oneline.input
+cat questions_oneline.input | awk -F'123thisisanewline123' '{$1=$1}1' OFS='\n' | awk ' { print "("$0")," } ' | sed "s/(/('/g" | sed "s/)/')/g" > questions_input.sql
